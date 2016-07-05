6c5e20153b91a59557ee03428006ed43c43264fe
宙生上村 <uemurachuusei@uemura-no-macbook-pro.local>
2016-07-05 19:02:25 +0900

    Initial Commit

---
+++ NaishoTalk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NaishoTalk
+//
+//  Created by 上村 宙生 on 2016/07/05.
+//  Copyright © 2016年 sectalk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
